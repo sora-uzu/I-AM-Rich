bb73361890dc2509f5249f485782c7e464bc1781
sora_uzu <naruto07170717@gmail.com>
2024-12-05 16:02:06 +0900

    first commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 照井宙 on 2024/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
